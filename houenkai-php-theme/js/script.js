8b684db4331c036a57c7f771760f50556c4cc5e9
mmmimo <gykyo0923@gmail.com>
2023-05-31 00:41:25 +0900

    2023/05/31

--- houenkai-php-theme/js/script.js
+++ houenkai-php-theme/js/script.js
@@ -1,7 +1,18 @@
-document.addEventListener(
-  "wpcf7mailsent",
-  function (event) {
-    location = "http://infiniadmin.xsrv.jp/houenkai-test/thanks-page/";
-  },
-  false
-);
+$(function () {
+  $(".hamburger").click(function () {
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $(".globalMenuSp").addClass("active");
+    } else {
+      $(".globalMenuSp").removeClass("active");
+    }
+  });
+});
+//メニュー内を閉じておく
+$(function () {
+  $(".globalMenuSp a[href]").click(function () {
+    $(".globalMenuSp").removeClass("active");
+    $(".hamburger").removeClass("active");
+  });
+});
